8fbff595c7b0debf3f024a3d5b3a7c8029c7934f
djd <dananjayahbi@gmail.com>
2024-06-10 11:43:11 +0530

    chore: Add uuid package as a dependency for generating unique IDs in the Gallery component

--- frontend/src/Pages/Gallery.jsx
+++ frontend/src/Pages/Gallery.jsx
@@ -1,28 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { Card, Row, Col, Spin } from "antd";
+import { Card, Row, Col, Spin, Empty, Image, Button, Modal } from "antd";
 import axios from "axios";
 import HeaderComp from "../components/HeaderComp";
 import Footer from "../components/Footer";
+import { DownloadOutlined, DeleteOutlined } from "@ant-design/icons";
+import { v4 as uuid } from "uuid";
 
 const Gallery = () => {
   const isLogged = window.localStorage.getItem("LoggedIn");
   if (!isLogged) {
-    // Redirect to login page if not logged in
     window.location.href = "/login";
   }
 
   const [galleryItems, setGalleryItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
+  const [selectedImageId, setSelectedImageId] = useState(null);
+  const userId = localStorage.getItem("userId");
 
   useEffect(() => {
     const fetchGalleryItems = async () => {
       try {
-        const userId = localStorage.getItem("userId");
         const response = await axios.get(
           `http://localhost:5000/api/users/getUserById/${userId}`
         );
         console.log(response.data);
         setGalleryItems(response.data.gallery);
+        // console.log(response.data.gallery);
       } catch (error) {
         console.error("Failed to fetch gallery items:", error);
       } finally {
@@ -33,6 +37,49 @@ const Gallery = () => {
     fetchGalleryItems();
   }, []);
 
+  const handleDownload = (url) => {
+    fetch(url)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob);
+        const downloadLink = document.createElement("a");
+        const uniqueId = uuid(); // Add this line to generate a unique ID
+        downloadLink.href = url;
+        downloadLink.download = `${uniqueId}.png`; // Modify the download filename
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(downloadLink);
+      })
+      .catch((error) => console.error("Error downloading image:", error));
+  };
+
+  const handleDelete = (imageId) => {
+    axios
+      .put(
+        `http://localhost:5000/api/users/deleteGalleryItem/${userId}/${imageId}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setGalleryItems(galleryItems.filter((item) => item._id !== imageId));
+      })
+      .catch((error) => console.error("Error deleting image:", error));
+  };
+
+  const showDeleteModal = (imageId) => {
+    setSelectedImageId(imageId);
+    setDeleteModalVisible(true);
+  };
+
+  const handleConfirmDelete = () => {
+    handleDelete(selectedImageId);
+    setDeleteModalVisible(false);
+  };
+
+  const handleCancelDelete = () => {
+    setDeleteModalVisible(false);
+  };
+
   if (loading) {
     return <Spin size="large" />;
   }
@@ -40,17 +87,59 @@ const Gallery = () => {
   return (
     <div>
       <HeaderComp />
-      <Row gutter={[16, 16]}>
-        {galleryItems.map((item, index) => (
-          <Col key={index} xs={24} sm={12} md={8}>
-            <Card
-              hoverable
-              cover={<img alt={`Gallery item ${index}`} src={item.url} />}
-            />
-          </Col>
-        ))}
-      </Row>
+      <div
+        style={{
+          minHeight: "calc(100vh - 120px)",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {galleryItems.length === 0 && <Empty />}
+        <div style={{ marginBottom: "50px" }}>
+          <h1 style={{ fontFamily: "sans-serif" }}>Your Gallery</h1>
+        </div>
+        <div style={{ width: "70%" }}>
+          <Row gutter={[16, 16]}>
+            {galleryItems.map((item, index) => (
+              <Col key={index} xs={24} sm={12} md={8}>
+                <Card
+                  hoverable
+                  cover={<Image alt={`Gallery item ${index}`} src={item.url} />}
+                  actions={[
+                    <Button
+                      type="link"
+                      icon={<DownloadOutlined />}
+                      onClick={() => handleDownload(item.url)}
+                    >
+                      Download
+                    </Button>,
+                    <Button
+                      danger
+                      type="link"
+                      icon={<DeleteOutlined />}
+                      onClick={() => showDeleteModal(item._id)}
+                    >
+                      Delete
+                    </Button>,
+                  ]}
+                />
+              </Col>
+            ))}
+          </Row>
+        </div>
+      </div>
       <Footer />
+
+      <Modal
+        title="Confirm Delete"
+        visible={deleteModalVisible}
+        onOk={handleConfirmDelete}
+        onCancel={handleCancelDelete}
+      >
+        <p>Are you sure you want to delete this image?</p>
+      </Modal>
     </div>
   );
 };
